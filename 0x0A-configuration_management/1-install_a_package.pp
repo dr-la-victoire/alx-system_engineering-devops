42d25f09782d9e054f9d8e146f27bd0533ba6fc0
dr-la-victoire <126200439+dr-la-victoire@users.noreply.github.com>
2023-12-01 19:14:28 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# This file installs a package (Flask) with pip3
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# This kills a process
-exec {'kill':
-  command => 'pkill killmenow',
-  path    => ['usr/bin', 'usr/sbin']
-}
