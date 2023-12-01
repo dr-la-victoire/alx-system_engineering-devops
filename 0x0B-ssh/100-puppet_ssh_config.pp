ac98c429a2d2a5afdd90b551be064a5666430cb3
dr-la-victoire <126200439+dr-la-victoire@users.noreply.github.com>
2023-12-01 19:14:48 +0100

    Delete 0x0B-ssh directory

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,9 +0,0 @@
-# This manifest enables connection to SSH without password auth
-
-file {'~/.ssh/config':
-  ensure  => 'present',
-  content => "Host myserver 
-	      HostName 100.26.231.31
-	      User ubuntu
-	      IdentityFile ~/.ssh/school"
-}
