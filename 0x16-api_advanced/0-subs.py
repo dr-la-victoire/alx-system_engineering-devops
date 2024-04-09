92f254526ac3d5206d79fd0b2f14617e8bbed7ca
La Victoire <lavictoire633@gmail.com>
2024-04-09 06:02:28 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""This module queries the Reddit API and returns the number of subscribers"""
+"""This module queries the Reddit API to get the number of subs a subreddit"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a sub reddit"""
-    url = 'http://reddit.com/r/{}/about.json'.format(subreddit)
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        results = response.json()
-        return results.get('data').get('subscribers')
-    else:
+    """Returns the number of subscribbers a subreddit has"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Chrome/58.0.3029.110 Safari/537.3'}
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        subs = data['data']['subscribers']
+        return subs
+    except Exception:
         return 0
