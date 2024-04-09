2f28a937444dd1dcb36e4318292863223cf71a83
La Victoire <lavictoire633@gmail.com>
2024-04-09 10:07:03 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,11 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribbers a subreddit has"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Chrome/98.0.4758.102'}
     try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        subs = data['data']['subscribers']
-        return subs
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        data = response.json().get('data')
+        results = data.get('subscribers')
+        return results
     except Exception:
         return 0
