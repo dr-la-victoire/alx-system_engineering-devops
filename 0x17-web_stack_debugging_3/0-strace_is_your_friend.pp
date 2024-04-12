ce5497217a25791086980adae54f0ef5fc4b0e5a
La Victoire <lavictoire633@gmail.com>
2024-04-12 10:23:12 -0700

    Solving the apache error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# A Puppet manifest that fixes the Apache error for the web stack project
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+file_line { 'replace_phpp_with_php':
+  path  => $file_to_edit,
+  line  => 'php',
+  match => 'phpp',
+}
+
