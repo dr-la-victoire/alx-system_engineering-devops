f1a26e04925b9702642bb4c2ffa98bc5fb984066
La Victoire <lavictoire633@gmail.com>
2024-04-19 12:59:10 -0700

    left off the path to the command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # This is a puppet manifest that kills a process named killmenow
 
 exec {'kills killmenow':
-  command => 'pkill killmenow'
+  command => 'pkill killmenow',
+  path    => ['usr/bin', 'usr/sbin']
 }
