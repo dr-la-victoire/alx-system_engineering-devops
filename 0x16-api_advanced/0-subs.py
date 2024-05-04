986f23e99881cfd3e5f993ab1415fd18c80e43a5
La Victoire <lavictoire633@gmail.com>
2024-05-04 14:15:55 -0700

    this code worked on my own computer

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""This module queries the Reddit API to get the number of subs a subreddit"""
+"""This module works with the Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribbers a subreddit has"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Chrome/98.0.4758.102'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        data = response.json().get('data')
-        results = data.get('subscribers')
-        return results
-    except Exception:
+    """This function gets the total number of subscribers to a subreddit"""
+    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 
+            'Mozilla/5.0 (X11; Linux i686; rv:124.0) Firefox/124.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
         return 0
+    data = response.json()
+    return data['data']['subscribers']
